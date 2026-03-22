3446383dccfe12e7c1e29c7dbf81f35afbc3159d
DevsManticore <a.tryhub1995@gmail.com>
2026-03-22 11:44:57 +0200

    Added menu ui scripts

---
+++ Assets/_Scripts/UI/MainMenu.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+namespace SharedCounter.Network
+{
+    public class MainMenu : MonoBehaviour
+    {
+        [SerializeField] private NetworkManagerLobby networkManager = null;
+
+        [Header("UI")]
+        [SerializeField] private GameObject inputNamePanel = null;
+        [SerializeField] private GameObject landingPagePanel = null;
+        [SerializeField] private GameObject hostLobbyButton = null;
+
+
+        private void OnEnable()
+        {
+            NetworkManagerLobby.OnClientDisconnected += HandleClientDisconnected;
+        }
+
+        private void OnDisable()
+        {
+            NetworkManagerLobby.OnClientDisconnected -= HandleClientDisconnected;
+        }
+
+        private void HandleClientDisconnected()
+        {
+            inputNamePanel.SetActive(true);
+            landingPagePanel.SetActive(false);
+        }
+
+        public void HostLobby()
+        {
+            networkManager.StartHost();
+
+            landingPagePanel.SetActive(false);
+        }
+    }
+}
