3cea191ae7841e8decef0f3678ef9d6ed7457fc1
DevsManticore <a.tryhub1995@gmail.com>
2026-03-22 12:08:39 +0200

    Implement network room player

--- Assets/_Scripts/Network/NetworkRoomPlayerLobby.cs
+++ Assets/_Scripts/Network/NetworkRoomPlayerLobby.cs
@@ -7,6 +7,115 @@ namespace SharedCounter.Network
 {
     public class NetworkRoomPlayerLobby : NetworkBehaviour
     {
+        [Header("UI")]
+        [SerializeField] private GameObject lobbyUI = null;
+        [SerializeField] private TMP_Text[] playerNameTexts = new TMP_Text[4];
+        [SerializeField] private TMP_Text[] playerReadyTexts = new TMP_Text[4];
+        [SerializeField] private Button startGameButton = null;
 
+        [SyncVar(hook = nameof(HandleDisplayNameChanged))]
+        public string DisplayName = "Loading...";
+        [SyncVar(hook = nameof(HandleReadyStatusChanged))]
+        public bool IsReady = false;
+
+        private NetworkManagerLobby room;
+        private NetworkManagerLobby Room
+        {
+            get
+            {
+                if (room != null) { return room; }
+                return room = NetworkManager.singleton as NetworkManagerLobby;
+            }
+        }
+
+        private void Awake()
+        {
+            SetVisibilityLobbyUI(false);
+            startGameButton.gameObject.SetActive(false);
+        }
+
+        public override void OnStartAuthority()
+        {
+            CmdTrySetDisplayName(PlayerNameInput.DisplayName);
+
+            if (isServer)
+            {
+                startGameButton.gameObject.SetActive(true);
+            }
+
+            SetVisibilityLobbyUI(true);
+        }
+
+        public override void OnStartClient()
+        {
+            Room.RoomPlayers.Add(this);
+
+            UpdateDisplay();
+        }
+
+        public override void OnStopClient()
+        {
+            Room.RoomPlayers.Remove(this);
+
+            UpdateDisplay();
+        }
+
+        public void HandleReadyStatusChanged(bool oldValue, bool newValue) => UpdateDisplay();
+        public void HandleDisplayNameChanged(string oldValue, string newValue) => UpdateDisplay();
+
+        private void UpdateDisplay()
+        {
+            if (!isOwned)
+            {
+                foreach (var player in Room.RoomPlayers)
+                {
+                    if (player.isOwned)
+                    {
+                        player.UpdateDisplay();
+                        break;
+                    }
+                }
+
+                return;
+            }
+
+            for (int i = 0; i < playerNameTexts.Length; i++)
+            {
+                playerNameTexts[i].text = "Waiting For Player...";
+                playerReadyTexts[i].text = string.Empty;
+            }
+
+            for (int i = 0; i < Room.RoomPlayers.Count; i++)
+            {
+                playerNameTexts[i].text = Room.RoomPlayers[i].DisplayName;
+                playerReadyTexts[i].text = Room.RoomPlayers[i].IsReady ?
+                    "<color=green>Ready</color>" :
+                    "<color=red>Not Ready</color>";
+            }
+        }
+
+        public void HandleReadyToStart(bool readyToStart)
+        {
+            startGameButton.interactable = readyToStart;
+        }
+
+        [Command]
+        private void CmdTrySetDisplayName(string displayName)
+        {
+            // TODO: add name validation
+
+            DisplayName = displayName;
+        }
+
+        [Command]
+        public void CmdStartGame()
+        {
+            Room.StartGame();
+        }
+
+        public void SetVisibilityLobbyUI(bool on)
+        {
+            lobbyUI.SetActive(on);
+        }
     }
 }
