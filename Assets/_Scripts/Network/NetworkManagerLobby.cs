664c06bbcdca73b64b92e83b496935bd69607c2c
DevsManticore <a.tryhub1995@gmail.com>
2026-03-22 11:26:34 +0200

    Added player join leave feature

---
+++ Assets/_Scripts/Network/NetworkManagerLobby.cs
@@ -0,0 +1,111 @@
+using Mirror;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace SharedCounter.Network
+{
+    public class NetworkManagerLobby : NetworkManager
+    {
+        [SerializeField] private int minPlayers = 2;
+
+        [Header("Room")]
+        [SerializeField] private NetworkRoomPlayerLobby roomPlayerPrefab = null;
+
+        public static event Action OnClientConnected;
+        public static event Action OnClientDisconnected;
+        public static event Action<NetworkConnection> OnServerReadied;
+        public static event Action OnServerStopped;
+
+        public List<NetworkRoomPlayerLobby> RoomPlayers { get; } = new List<NetworkRoomPlayerLobby>();
+
+        public override void OnStartServer()
+        {
+            base.OnStartServer();
+
+            spawnPrefabs = Resources.LoadAll<GameObject>("SpawnablePrefabs").ToList();
+        }
+
+        public override void OnStartClient()
+        {
+            var spawnablePrefabs = Resources.LoadAll<GameObject>("SpawnablePrefabs");
+
+            foreach (var prefab in spawnablePrefabs)
+            {
+                NetworkClient.RegisterPrefab(prefab);
+            }
+        }
+
+        public override void OnClientConnect()
+        {
+            base.OnClientConnect();
+
+            OnClientConnected?.Invoke();
+        }
+
+        public override void OnClientDisconnect()
+        {
+            base.OnClientDisconnect();
+
+            OnClientDisconnected?.Invoke();
+        }
+
+        public override void OnServerConnect(NetworkConnectionToClient conn)
+        {
+            if (numPlayers >= maxConnections)
+            {
+                conn.Disconnect();
+                return;
+            }
+        }
+
+        public override void OnServerAddPlayer(NetworkConnectionToClient conn)
+        {
+            NetworkRoomPlayerLobby roomPlayerInstance = Instantiate(roomPlayerPrefab);
+            NetworkServer.AddPlayerForConnection(conn, roomPlayerInstance.gameObject);
+        }
+
+        public override void OnServerDisconnect(NetworkConnectionToClient conn)
+        {
+            if (conn.identity != null)
+            {
+                var player = conn.identity.GetComponent<NetworkRoomPlayerLobby>();
+
+                RoomPlayers.Remove(player);
+
+                NotifyPlayersOfReadyState();
+            }
+
+            base.OnServerDisconnect(conn);
+        }
+
+        public override void OnStopServer()
+        {
+            OnServerStopped?.Invoke();
+
+            RoomPlayers.Clear();
+        }
+
+        public void NotifyPlayersOfReadyState()
+        {
+            foreach (var player in RoomPlayers)
+            {
+                // TODO: add HandleReadyToStart on players
+            }
+        }
+
+        public void StartGame()
+        {
+            // TODO: add start game
+        }
+
+        public override void OnServerReady(NetworkConnectionToClient conn)
+        {
+            base.OnServerReady(conn);
+
+            OnServerReadied?.Invoke(conn);
+        }
+    }
+}
+
---
+++ Assets/_Scripts/Network/NetworkRoomPlayerLobby.cs
@@ -0,0 +1,12 @@
+using Mirror;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace SharedCounter.Network
+{
+    public class NetworkRoomPlayerLobby : NetworkBehaviour
+    {
+
+    }
+}
